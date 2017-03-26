c29fd794324dc3cfe4129e8a7c140e4736e29b24
Kathryn <kathryn.mckay@uoit.net>
2017-03-25 20:02:31 -0400

    A cute mosaic demo with averaged pixels.

--- cpp/imosaic.cpp
+++ cpp/imosaic.cpp
@@ -2,29 +2,52 @@
 #include <opencv2/opencv.hpp>
 #include "mosaicbuilder.hpp"
 
+struct trackbar_data_t {
+  const cv::Mat* orig_hsv;
+  cv::Mat dest;
+  std::string window;
+};
+
+void buildMosaic(trackbar_data_t* trackbar_data, int tile_length) {
+  using namespace imosaic;
+  Mosaic<GridCutter<NonUniformSplitter>, AverageTiler> mosaic;
+  mosaic.setTilesize(cv::Size(tile_length,tile_length));
+  mosaic.build(*trackbar_data->orig_hsv, &trackbar_data->dest);
+}
+
+void onTrackbar(int tile_length, void* p_trackbar_data) {
+  trackbar_data_t* trackbar_data = (trackbar_data_t*)p_trackbar_data;
+  buildMosaic(trackbar_data, tile_length);
+  cv::cvtColor(trackbar_data->dest, trackbar_data->dest, CV_HSV2BGR);
+  cv::imshow(trackbar_data->window, trackbar_data->dest);
+}
+
 int main(int argc, char **argv){
+
+  // Print usage if arguments are malformed
   if(argc != 2){
     printf("usage: %s [Image File]\n",argv[0]);
     return -1;
   }
 
-  const cv::Mat image = cv::imread(argv[1],1);
-
+  // Load in HSV image.
+  cv::Mat image = cv::imread(argv[1],1);
   if(!image.data){
     printf("No image data\n");
     return -1;
   }
+  cv::cvtColor(image, image, CV_BGR2HSV);
 
-  std::unordered_map<std::string, imosaic::ImageFeatures> queror;
-
-  const cv::Mat result = imosaic::createMosaic(image
-    , imosaic::UnorderedMap<std::string, imosaic::ImageFeatures>(queror)
-    , cv::Size(32,32));
-
-  cv::namedWindow("Result",cv::WINDOW_AUTOSIZE);
-  cv::imshow("Result",result);
-
-  cv::waitKey(0);
-
+  // display result
+  trackbar_data_t trackbar_data{&image, cv::Mat::zeros(image.size(), CV_8UC3), "Result"};
+  int tilesize = 16;
+  int max_pixel_size = std::fmin(image.size().width, image.size().height)/2;
+  cv::namedWindow(trackbar_data.window,cv::WINDOW_AUTOSIZE);
+  cv::createTrackbar("Threshold", trackbar_data.window
+    , &tilesize, max_pixel_size
+    , onTrackbar, &trackbar_data);
+  cv::setTrackbarMin("Threshold", trackbar_data.window, 8);
+  onTrackbar(tilesize, &trackbar_data);
+  cv::waitKey();
   return 0;
 }
--- cpp/mosaicbuilder.hpp
+++ cpp/mosaicbuilder.hpp
@@ -6,61 +6,125 @@
 
 #include <unordered_map>
 #include <opencv2/opencv.hpp>
-#include "features.hpp"
 
 namespace imosaic {
-/// wrapper around unordered_map.
-/// because templates, that's why.
-template<typename K, typename V>
-class UnorderedMap {
+/// splits area into cells defined by 'tilesize'.
+/// If size % tilesize != 0, the extra area is discarded.
+/// When tilesize's area is zero, the entire field is returned.
+template<typename T>
+class UniformSplitter {
 private:
-  std::unordered_map<K, V> map_;
-public:
-  UnorderedMap(const std::unordered_map<K, V>& map) : map_(map) {}
-  const V& query(const K& k) const { return map_[k]; }
-  void put(const K& k, V&& v) { map_[k] = v; }
-};
+  cv::Size tilesize_;
 
-/// Creates a mosaic from hsv using components from bank split into uniformly
-///   aligned tiles.
-class GridMosaicBuilder {
-private:
-  cv::Size tile_size_;
+protected:
+  ~UniformSplitter() {}
 
 public:
-  GridMosaicBuilder(cv::Size tile_size) : tile_size_(tile_size) {}
-
-  template<template <class, class> typename Queror>
-  cv::Mat build(const cv::Mat& hsv
-    , const Queror<std::string, ImageFeatures>& bank) {
-      cv::Size canvas_size = hsv.size();
-      cv::Mat result = cv::Mat::zeros(canvas_size, hsv.type());
-      for(int i = 0; i < canvas_size.height; i++) {
-        for(int j = 0; j < canvas_size.width; j++) {
-            //int ip = i/tile_size_.height;
-            //int jp = j/tile_size_.width;
-            result.at<double>(i, j) = hsv.at<double>(i,j);
-        }
-      }
-      return result;
-    }
-};
-
-/// createMosaic
-/// hsv:      Image matrix, with HSV cells
-/// bank:     Queror policy using filenames for keys and ImageFeatures for values.
-/// tilesize: x/y length, in pixels, of each grid cell
-///
-/// Returns assembled mosaic image, coded as HSV.
-template <template <class, class> typename Queror>
-cv::Mat createMosaic(const cv::Mat& hsv
-  , const Queror<std::string, ImageFeatures>& bank
-  , cv::Size tilesize) {
-    GridMosaicBuilder builder(tilesize);
-    return builder.build(hsv, bank);
+  void setTilesize(cv::Size tilesize) {
+    tilesize_ = tilesize;
   }
 
-}
+protected:
+  std::vector<T> split(cv::Size size) {
+    std::vector<T> list;
+    cv::Size tilesize(tilesize_.area() == 0 ? cv::Size(1,1) : tilesize_);
+    cv::Size cell_count(size.width/tilesize.width, size.height/tilesize.height);
+    cv::Size offset(size.width - tilesize.width*cell_count.width, size.height - tilesize.height*cell_count.height);
+    offset = offset/2;
+    for(int i = 0; i < cell_count.height; i++) {
+      for(int j = 0; j < cell_count.width; j++) {
+        list.push_back(T(offset.width + tilesize.width*j, offset.height + tilesize.height*i, tilesize.width
+          , tilesize.height));
+      }
+    }
+    return list;
+  }
+};
 
+/// splits area into cells defined by 'tilesize'.
+/// If size % tilesize != 0, certain cells are truncated.
+template<typename T>
+class NonUniformSplitter {
+private:
+  cv::Size tilesize_;
+
+protected:
+  ~NonUniformSplitter() {}
+
+public:
+  void setTilesize(cv::Size tilesize) {
+    tilesize_ = tilesize;
+  }
+
+protected:
+  std::vector<T> split(cv::Size size) {
+    std::vector<T> list;
+    cv::Size tilesize(tilesize_.area() == 0 ? cv::Size(1,1) : tilesize_);
+    cv::Size cell_count(1+(size.width/tilesize.width), size.height/tilesize.height);
+    cv::Size offset;
+    for(int i = 0; i < cell_count.height; i++) {
+      cv::Size cell_size(tilesize);
+      if(i == cell_count.height/2) {
+        cell_size.height = size.height - (cell_count.height-1)*tilesize.height;
+        if(cell_size.height == 0)
+          continue;
+      }
+      for(int j = 0; j < cell_count.width; j++) {
+        cell_size.width = tilesize.width;
+        if(j == cell_count.width/2) {
+          cell_size.width = size.width - (cell_count.width-1)*tilesize.width;
+          if(cell_size.width == 0)
+            continue;
+        }
+        list.push_back(T(offset, cell_size));
+        offset.width += cell_size.width;
+      }
+      offset.width = 0;
+      offset.height += cell_size.height;
+    }
+
+    return list;
+  }
+};
+
+template<template <typename> typename SplitterPolicy>
+class GridCutter : public SplitterPolicy<cv::Rect2i> {
+protected:
+  ~GridCutter() {}
+protected:
+  std::vector<cv::Mat> cutUp(const cv::Mat& mat) {
+    std::vector<cv::Mat> cells;
+    std::vector<cv::Rect2i> subrects(SplitterPolicy<cv::Rect2i>::split(mat.size()));
+    std::for_each(subrects.begin(), subrects.end(), [&cells, &mat]
+      (cv::Rect2i roi) {
+      cells.push_back(cv::Mat(mat, roi));
+    });
+    return cells;
+  }
+};
+
+class AverageTiler {
+protected:
+  ~AverageTiler() {}
+protected:
+  void replaceRegion(const cv::Mat& source, cv::Mat& dest) {
+    dest = cv::mean(source);
+  }
+};
+
+template <typename CutterPolicy, typename TilerPolicy>
+class Mosaic : public CutterPolicy, public TilerPolicy  {
+public:
+  void build(const cv::Mat& source_hsv, cv::Mat* dest_hsv) {
+    *dest_hsv = 0;
+    std::vector<cv::Mat> dest_regions(CutterPolicy::cutUp(*dest_hsv));
+    std::vector<cv::Mat> source_regions(CutterPolicy::cutUp(source_hsv));
+    size_t size = dest_regions.size();
+    for(int i = 0; i < size; i++) {
+      TilerPolicy::replaceRegion(source_regions[i], dest_regions[i]);
+    }
+  }
+};
+}
 
 #endif //IMOSAIC_MOSAIC_BUILDER_HPP
