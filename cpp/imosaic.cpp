004e0aa801ec349e3021dee4b5e0f75284c8c96e
Santiago Bonada <santiago.bonada@uoit.net>
2017-03-26 13:02:03 -0400

    moved metafile and queror testing code to seperate source file/executable

--- cpp/imosaic.cpp
+++ cpp/imosaic.cpp
@@ -16,20 +16,6 @@ int main(int argc, char **argv){
     return -1;
   }
 
-  /* test metafile */
-  imosaic::MetaFile meta1(0,argv[2]);
-  meta1.print();
-
-  /* test queror */
-  imosaic::Queror queror1(meta1);
-  cv::Mat test(1,3,CV_32F);
-  test.at<float>(0,0) = 60.0f;
-  test.at<float>(0,1) = 120.0f;
-  test.at<float>(0,2) = 115.0f;
-  imosaic::Query query1 = queror1.query(test);
-
-  printf("Query returned %s as a suitable candidate with distance of %f\n",query1.filename.c_str(),query1.distance);
-
   cv::namedWindow("Image",cv::WINDOW_AUTOSIZE);
   cv::imshow("Image",image);
 
